62c0f6b94774561f52e41a1d3caa4f4028387359
Patrick Whelan <patrickawhelan@gmail.com>
2020-03-25 14:13:59 -0400

    finished code

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,3 +1,25 @@
-public class Teacher {
+// create a teacher class based on the data in profiles.json
 
+public class Teacher {
+  private String name;
+  private String favoriteLanguage;
+  private int yearsExperience;
+
+  public Teacher(String name, String favoriteLanguage, int yearsExperience) {
+    this.name = name;
+    this.favoriteLanguage = favoriteLanguage;
+    this.yearsExperience = yearsExperience;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getFavoriteLanguage() {
+    return favoriteLanguage;
+  }
+
+  public int getYearsExperience() {
+    return yearsExperience;
+  }
 }
--- src/main/java/TeacherProfile.java
+++ src/main/java/TeacherProfile.java
@@ -8,20 +8,49 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+
 public class TeacherProfile {
   public static void main(String[] args) throws IOException {
     // put the teachers in an unstructured/String based ArrayList
+    File teacherProfilesJson = new File(TeacherProfile.class.getResource("profiles.json").getFile());
+    List<HashMap<String,String>> teachers;
+    ObjectMapper mapper = new ObjectMapper();
+    teachers = mapper.readValue(teacherProfilesJson, ArrayList.class);
 
+    System.out.println(teachers);
+    lineBreak();
 
     // put the teachers in a structured ArrayList of teacher objects
-
+    List<Teacher> teacherList = new ArrayList<Teacher>();
+    for (Map<String,String> teacherMap : teachers) {
+      Teacher teacher = new Teacher(teacherMap.get("name"), teacherMap.get("favorite_language"), Integer.parseInt(teacherMap.get("years_experience")));
+      teacherList.add(teacher);
+    }
+    System.out.println(teacherList);
+    lineBreak();
 
     // list out all of the teachers names
-
-    // list out favorite languages
-
+    for (Teacher teacher: teacherList) {
+      System.out.println(teacher.getName());
+    }
+    lineBreak();
+    // list out favorite languages without duplication
+    Set<String> favoriteLanguages = new HashSet<String>();
+    for (Teacher teacher : teacherList) {
+      favoriteLanguages.add(teacher.getFavoriteLanguage());
+    }
+    System.out.println(String.join(", ", favoriteLanguages));
+    lineBreak();
 
     // sum up the years of experience on the team
+    int totalYearsExperience = 0;
+    for (Teacher teacher: teacherList) {
+      totalYearsExperience += teacher.getYearsExperience();
+    }
+    System.out.println("Total Years Experience: " + totalYearsExperience);
+  }
 
+  private static void lineBreak(){
+    System.out.println("\n---\n");
   }
 }
