2e8e1035eb57644d2b855c0f870a9b32203758e0
Patrick Whelan <patrickawhelan@gmail.com>
2020-03-25 10:36:15 -0400

    removed code to create starter branch

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,24 +1,3 @@
 public class Teacher {
 
-  private final String name;
-  private final String favoriteLanguage;
-  private final int yearsOfExperience;
-
-  public Teacher(String name, String favoriteLanguage, int yearsOfExperience) {
-    this.name = name;
-    this.favoriteLanguage = favoriteLanguage;
-    this.yearsOfExperience = yearsOfExperience;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getFavoriteLanguage() {
-    return favoriteLanguage;
-  }
-
-  public int getYearsOfExperience() {
-    return yearsOfExperience;
-  }
 }
--- src/main/java/TeacherProfile.java
+++ src/main/java/TeacherProfile.java
@@ -11,36 +11,17 @@ import java.util.Set;
 public class TeacherProfile {
   public static void main(String[] args) throws IOException {
     // put the teachers in an unstructured/String based ArrayList
-    File teacherProfilesJson = new File(TeacherProfile.class.getResource("profiles.json").getFile());
-    List<HashMap<String, String>> teachers;
-    ObjectMapper mapper = new ObjectMapper();
-    teachers = mapper.readValue(teacherProfilesJson, ArrayList.class);
+
 
     // put the teachers in a structured ArrayList of teacher objects
-    List<Teacher> teacherList = new ArrayList<Teacher>();
-    for (Map<String, String> teacherMap : teachers) {
-      Teacher teacher = new Teacher(teacherMap.get("name"), teacherMap.get("favorite_language"),
-          Integer.parseInt(teacherMap.get("years_experience")));
-      teacherList.add(teacher);
-    }
+
 
     // list out all of the teachers names
-    for (Teacher teacher : teacherList) {
-      System.out.println(teacher.getName());
-    }
 
     // list out favorite languages
-    Set<String> favoriteLanguages = new HashSet<String>();
-    for (Teacher teacher : teacherList) {
-      favoriteLanguages.add(teacher.getFavoriteLanguage());
-    }
-    System.out.println(String.join(", ", favoriteLanguages));
+
 
     // sum up the years of experience on the team
-    int totalYearsExperience = 0;
-    for (Teacher teacher : teacherList) {
-      totalYearsExperience += teacher.getYearsOfExperience();
-    }
-    System.out.println(totalYearsExperience);
+
   }
 }
