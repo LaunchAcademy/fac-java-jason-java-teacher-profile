55be28761665e288e723ad476107745422040195
KevinTranLA <kevin.tran@launchacademy.co>
2021-04-13 09:07:38 -0700

    final code from fac

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,3 +1,28 @@
 public class Teacher {
 
+    private String name;
+    private String favoriteLanguage;
+    private int yearsExperience;
+
+    public Teacher(String name, String favoriteLanguage, int yearsExperience) {
+        this.name = name;
+        this.favoriteLanguage = favoriteLanguage;
+        this.yearsExperience = yearsExperience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public int getYearsExperience() {
+        return yearsExperience;
+    }
+
+    public String toString() {
+        return this.name + " " + this.favoriteLanguage + " " + this.yearsExperience;
+    }
 }
--- src/main/java/TeacherProfile.java
+++ src/main/java/TeacherProfile.java
@@ -1,20 +1,54 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class TeacherProfile {
   public static void main(String[] args) throws IOException {
     // put the teachers in an unstructured/String based ArrayList
     File profilesJson = new File("src/main/resources/profiles.json");
+    List<HashMap<String, String>> teachers;
+    teachers = new ArrayList<HashMap<String, String>>();
+
+    ObjectMapper mapper = new ObjectMapper();
+    teachers = mapper.readValue(profilesJson, ArrayList.class);
+
+    System.out.println("Unstructured: " + teachers);
+    System.out.println("=======");
 
     // put the teachers in a structured ArrayList of teacher objects
+    List<Teacher> teachersObj = new ArrayList<Teacher>();
+    for(Map<String, String> curTeacher : teachers) {
+      Teacher singleTeacher = new Teacher(curTeacher.get("name"), curTeacher.get("favorite_language"),
+        Integer.parseInt(curTeacher.get("years_experience")));
+      teachersObj.add(singleTeacher);
+    }
 
+    System.out.println("Structured: " + teachersObj);
+    System.out.println("=======");
 
     // list out all of the teachers names
+    for(Teacher teacher : teachersObj){
+      System.out.println(teacher.getName());
+    }
 
+    System.out.println("=======");
     // list out favorite languages
+    Set<String> favoriteLanguageSet = new HashSet<String>();
+    for(Teacher teacher : teachersObj){
+//      System.out.println(teacher.getFavoriteLanguage());
+        favoriteLanguageSet.add(teacher.getFavoriteLanguage());
+    }
+    System.out.println(favoriteLanguageSet);
 
 
     // sum up the years of experience on the team
+    int yearsSummation = 0;
+    for(Teacher teacher : teachersObj){
+      yearsSummation += teacher.getYearsExperience();
+    }
+    System.out.println(yearsSummation);
 
   }
 }
